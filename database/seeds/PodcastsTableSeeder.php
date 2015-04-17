10e579ff167541ac468be529189c19a040895b1f
Plesca Dragos <plescadragoss@yahoo.com>
2015-04-17 15:41:32 +0300

    Small seeder change

--- database/seeds/PodcastsTableSeeder.php
+++ database/seeds/PodcastsTableSeeder.php
@@ -14,7 +14,7 @@ class PodcastsTableSeeder extends Seeder {
         	[
         		'title' => 'Invisibilia',
         		'description' => 'Invisibilia (Latin for all the invisible things) is about the invisible forces that control human behavior – ideas, beliefs, assumptions and emotions. Co-hosted by Lulu Miller and Alix Spiegel, Invisibilia interweaves narrative storytelling with scientific research that will ultimately make you see your own life differently.',
-        		'image' => 'http://media.npr.org/images/podcasts/primary/icon_510307-b8676d21f76d8a78f9881171c28ae6569f8b795b.jpg?s=200',
+        		'image' => 'http://media.npr.org/images/podcasts/primary/icon_510307-b8676d21f76d8a78f9881171c28ae6569f8b795b.jpg?s=1400',
         		'url' => 'http://www.npr.org/rss/podcast.php?id=510307',
         		'active' => 1
         	],
