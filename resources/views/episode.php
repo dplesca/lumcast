4bdda20e8e940536d2f19b0c73d4bc90ea79d401
Plesca Dragos <plescadragoss@yahoo.com>
2015-04-17 16:08:38 +0300

    even more layout work

--- resources/views/episode.php
+++ resources/views/episode.php
@@ -8,6 +8,7 @@
 	<link href="//fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="/css/normalize.css">
 	<link rel="stylesheet" href="/css/skeleton.css">
+	<link rel="stylesheet" href="/css/custom.css">
 	<link href="//vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
 	<script src="//vjs.zencdn.net/4.12/video.js"></script>
 	<style>
@@ -16,14 +17,14 @@
 </head>
 <body>
 	<div class="container">
-		<div class="row" style="margin-top:20px;">
-			<div class="nine column">
-				<h3><a href="/<?= str_slug($podcast->title) ?>-<?= $podcast->id ?>"><?= $podcast->title ?></a></h3>
+		<div class="row logo-row">
+			<div class="nine columns">
+				<h1><a class="logo" href="/">lumcast</a></h1>
 			</div>
 		</div>
 		<div class="row">
-			<div class="two-thirds column">
-				<h4><?= $episode->title ?></h4>
+			<div class="nine columns">
+				<h3><a href="/<?= str_slug($podcast->title) ?>-<?= $podcast->id ?>"><?= $podcast->title ?></a> - <?= $episode->title ?></h3>
 				<p><?= $episode->description ?></p>
 				<audio id="example_video_1" class="video-js vjs-default-skin vjs-big-play-centered" preload="auto" controls width="340" height="340" poster="<?= $podcast->image?>" data-setup='{}'>
 					<source src="<?= $episode->mp3_url ?>" type="audio/mp3">
--- resources/views/podcast.php
+++ resources/views/podcast.php
@@ -8,15 +8,19 @@
 	<link href="//fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="/css/normalize.css">
 	<link rel="stylesheet" href="/css/skeleton.css">
-	<style>
-	h3{font-weight: 600;}
-	</style>
+	<link rel="stylesheet" href="/css/custom.css">
 </head>
 <body>
 	<div class="container">
+		<div class="row logo-row">
+			<div class="nine columns">
+				<h1><a class="logo" href="/">lumcast</a></h1>
+			</div>
+		</div>
 		<div class="row">
 			<div class="two-thirds column" style="margin-top:20px;">
 				<h3><?= $podcast->title ?></h3>
+				<p><img src="<?= $podcast->image ?>" class="u-pull-left left-image" width="200" alt=""><?= $podcast->description ?></p>
 				<ul>
 					<?php foreach($episodes as $key => $episode): ?>
 					<li><a href="/<?= str_slug($podcast->title, '-')?>-<?= $podcast->id ?>/<?= str_slug($episode->title, '-')?>-<?= $episode->id ?>"><?= $episode->title ?></a> - <?= $episode->description ?></li>
