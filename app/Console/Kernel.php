9d247c0ec25300a88bb2dfcb20b2505bda943426
Dragos Plesca <dragos.plesca@gmail.com>
2015-04-20 08:22:26 +0300

    schedule fetch command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('fetch')
+                 ->hourly();
     }
 }
