35867a0e792813b3cc0865c7c0579a1de7f5669d
Dragos Plesca <dragos.plesca@gmail.com>
2015-05-07 18:20:16 +0300

    Relationship between podcast and episode

--- app/Episode.php
+++ app/Episode.php
@@ -1,4 +1,11 @@
 <?php namespace App;
 use Illuminate\Database\Eloquent\Model;
 
-class Episode extends Model {}
+class Episode extends Model {
+
+	public function podcast()
+    {
+        return $this->belongsTo('App\Podcast');
+    }
+
+}
--- app/Podcast.php
+++ app/Podcast.php
@@ -3,4 +3,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Podcast extends Model {
 	protected $fillable = ['title', 'description', 'url', 'image', 'active'];
+
+	public function episodes()
+    {
+        return $this->hasMany('App\Episode');
+    }
 }
